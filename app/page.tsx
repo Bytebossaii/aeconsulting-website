92138094d13fc0508a28044c907c7c3438d2ad71
Your Name <you@example.com>
2026-03-26 12:19:03 +0100

    Use code.html as live homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,5 @@
-"use client"
-
-import { LanguageProvider } from "@/lib/language-context"
-import { Navbar } from "@/components/navbar"
-import { Hero } from "@/components/hero"
-import { TrustBar } from "@/components/trust-bar"
-import { PainPoints } from "@/components/pain-points"
-import { Services } from "@/components/services"
-import { HowItWorks } from "@/components/how-it-works"
-import { About } from "@/components/about"
-import { FAQ } from "@/components/faq"
-import { CTAStrip } from "@/components/cta-strip"
-import { Contact } from "@/components/contact"
-import { Footer } from "@/components/footer"
+import { redirect } from "next/navigation"
 
 export default function Home() {
-  return (
-    <LanguageProvider>
-      <Navbar />
-      <main>
-        <Hero />
-        <TrustBar />
-        <PainPoints />
-        <Services />
-        <HowItWorks />
-        <About />
-        <section id="faq">
-          <FAQ />
-        </section>
-        <CTAStrip />
-        <Contact />
-      </main>
-      <Footer />
-    </LanguageProvider>
-  )
+  redirect("/code.html")
 }
